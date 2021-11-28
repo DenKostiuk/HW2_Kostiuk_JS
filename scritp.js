609f67eb710ccecd0c2dee83eb651e130bbaa195
Denys Kostiuk <93026512+DenKostiuk@users.noreply.github.com>
2021-11-28 16:18:11 +0200

    3

--- scritp.js
+++ scritp.js
@@ -1,8 +1,7 @@
-let numbers = [25, 1];
-let project = 'проект';
-let team = 'команда';
-let howMuch = 'Сколько';
-let sentence = 'нужно программистов чтобы сделать проект ?';
-let result = howMuch + ' ' + sentence + ' ' + project.slice(0, 1).toUpperCase() + project.slice(1) + ' ' + numbers[1] + ' ' + team.slice(0, 1).toUpperCase() + team.slice(1) + ' ' + numbers[0] + '.';
+function oddOrEven (number) {
+    return (-1) ** number + 1 ? 'Четное' : 'Нечетное';
+}
 
-console.log(result) // --> собрать предложение
+let number = +prompt('Введите число');
+
+alert(oddOrEven(number));
